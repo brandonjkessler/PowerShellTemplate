c825e433bcd4845f2a04fac505ca0b7b3b2d1fb7
Brandon Kessler <brandon.kessler@state.co.us>
2022-07-26 13:06:16 -0600

    FEAT: Adds more comments

--- PowerShellTemplate.ps1
+++ PowerShellTemplate.ps1
@@ -5,16 +5,18 @@ param(
 )
 
 begin{
-    # BEGIN: Executes First. Executes once. Useful for setting up and initializing. Optional
+    #-- BEGIN: Executes First. Executes once. Useful for setting up and initializing. Optional
     if($LogPath -match '\\$'){
         $LogPath = $LogPath.Substring(0,($LogPath.Length - 1))
     }
     Write-Verbose -Message "Creating log file at $LogPath."
-    # Use Start-Transcript to create a .log file
+    #-- Use Start-Transcript to create a .log file
+    #-- If you use "Throw" you'll need to use "Stop-Transcript" before to stop the logging.
+    #-- Major Benefit is that Start-Transcript also captures -Verbose and -Debug messages.
     Start-Transcript -Path "$LogPath\NameofScript.log"
 }
 process{
-    # PROCESS: Executes second. Executes multiple times based on how many objects are sent to the function through the pipeline. Optional.
+    #-- PROCESS: Executes second. Executes multiple times based on how many objects are sent to the function through the pipeline. Optional.
 }
 end{
     # END: Executes Once. Executes Last. Useful for all things after process, like cleaning up after script. Optional.
